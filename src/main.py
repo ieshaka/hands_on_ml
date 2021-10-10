a63f02fddc6a9ea4e4ca2570d2e65e5931d5882f
WarnaK <ieshaka.jkh@keells.com>
2021-10-10 16:55:23 +0530

    adding the train_test_spilt.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,23 @@
 this is the main pipeline of the code
 """
 from common import utilities as ut
+from model import train_test_spilt
+import numpy as np
 
 
 def main():
-    housing_data = ut.load_data(r'C:\Users\WarnaK\OneDrive - John Keells Holdings PLC\workspace\python_train\hands_on_ml\hands_on_ml\Data\housing\housing.csv')
+    housing_data = ut.load_data(r'../Data/housing/housing.csv')
     print(housing_data.shape)
+    train_data, val_data, test_data = train_test_spilt.stratified_spilt(
+        housing_data,
+        "median_income",
+        [0., 1.5, 3.0, 4.5, 6, np.inf],
+        [1, 2, 3, 4, 5],
+        0.2,
+        0.1,
+        True
+    )
+    print(test_data.shape)
 
 
 if __name__ == '__main__':
---
+++ src/model/train_test_spilt.py
@@ -0,0 +1,51 @@
+"""
+this file contains the fuctions to break the dataset into train,test and validation
+"""
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
+def stratified_spilt(
+        data: pd.DataFrame,
+        stra_col: str,
+        bins: list,
+        labels: list,
+        test_size: float,
+        val_size: float,
+        is_numerical: bool
+) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    """
+    This functions breaks the dataset into train test and validation based on
+    the stra_col and the test and val sizes
+    :param data: dataframe of the dataset
+    :param stra_col: column name that the strata is created on
+    :param bins: list of bins if the strata column is continues
+    :param labels: list of labels for the bins
+    :param test_size: size of the test set
+    :param val_size: size of the train set
+    :param is_numerical: if the stratifying column is continues
+    :return:
+    """
+    if is_numerical:
+        data['strata_cat'] = pd.cut(data[stra_col], bins=bins, labels=labels)
+    else:
+        data['strata_cat'] = data[stra_col]
+
+    test_split = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=42)
+
+    for train_index, test_index in test_split.split(data, data['strata_cat']):
+        start_train_set = data.loc[train_index]
+        start_test_set = data.loc[test_index]
+
+    val_split = StratifiedShuffleSplit(n_splits=1, test_size=val_size, random_state=42)
+
+    start_train_set_reindex = start_train_set.reset_index().drop('index', axis=1)
+
+    for train_index, val_index in val_split.split(start_train_set_reindex, start_train_set_reindex['strata_cat']):
+        start_train_set = start_train_set_reindex.loc[train_index]
+        start_val_set = start_train_set_reindex.loc[val_index]
+
+    for data_ in (start_train_set, start_val_set, start_test_set):
+        data_.drop('strata_cat', axis=1, inplace=True)
+
+    return start_train_set, start_val_set, start_test_set
